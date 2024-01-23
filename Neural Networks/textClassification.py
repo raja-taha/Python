dad1cf48a2976b9b34252214d584b2704fc3be77
Mr_RajaG <rajataha062@gmail.com>
2024-01-23 07:14:27 +0500

    Complete model for Text Classifiaction

--- Neural Networks/textClassification.py
+++ Neural Networks/textClassification.py
@@ -4,7 +4,7 @@ import numpy as np
 
 data = keras.datasets.imdb
 
-(train_data, train_labels), (test_data, test_labels) = data.load_data(num_words=10000)
+(train_data, train_labels), (test_data, test_labels) = data.load_data(num_words=88000)
 
 word_index = data.get_word_index()
 word_index = {k:(v+3) for k,v in word_index.items()}
@@ -21,10 +21,55 @@ test_data = keras.preprocessing.sequence.pad_sequences(test_data, value=word_ind
 def decode_review(text):
     return " ".join([reverse_word_index.get(i, "?") for i in text])
 
-model = keras.Sequential()
-model.add(keras.layers.Embedding(10000, 16))
-model.add(keras.layers.GlobalAveragePooling1D())
-model.add(keras.layers.Dense(16, activation="relu"))
-model.add(keras.layers.Dense(1, activation="sigmod"))
+# model = keras.Sequential()
+# model.add(keras.layers.Embedding(88000, 16))
+# model.add(keras.layers.GlobalAveragePooling1D())
+# model.add(keras.layers.Dense(16, activation="relu"))
+# model.add(keras.layers.Dense(1, activation="sigmoid"))
+#
+# model.summary()
+#
+# model.compile(optimizer="adam", loss="binary_crossentropy", metrics=["accuracy"])
+#
+# x_val = train_data[:10000]
+# x_train = train_data[10000:]
+#
+# y_val = train_labels[:10000]
+# y_train = train_labels[10000:]
+#
+# fitModel = model.fit(x_train, y_train, epochs=40, batch_size=512, validation_data=(x_val, y_val), verbose=1)
+#
+# results = model.evaluate(test_data, test_labels)
+# print(results)
+#
+# model.save("textClassification.h5")
 
-model.summary()
+def review_encode(s):
+    encoded = [1]
+
+    for word in s:
+        if word.lower() in word_index:
+            encoded.append(word_index[word.lower()])
+        else:
+            encoded.append(2)
+
+    return encoded
+
+model = keras.models.load_model('textClassification.h5')
+
+with open ("testReview.txt", encoding="utf-8") as f:
+    for line in f.readlines():
+        nline = line.replace(",", "").replace(".", "").replace("(", "").replace(")", "").replace(":", "").replace("\'", "").strip().split(" ")
+        encode = review_encode(nline)
+        encode = keras.preprocessing.sequence.pad_sequences([encode], value=word_index["<PAD>"], padding="post", maxlen=250)
+        predict = model.predict(encode)
+        print(line)
+        print(encode)
+        print(predict[0])
+
+# test_review = test_data[0]
+# predict = model.predict(np.array([test_review]))
+# print("Review: ")
+# print(decode_review(test_review))
+# print("Prediction: " + str(predict[0]))
+# print("Actual: " + str(test_labels[0]))
